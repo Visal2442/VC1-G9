d50a628ba9c4b2f625621f7950b36b9237e12554
Visal Sork <visalsork@Visals-MacBook-Air.local>
2023-01-23 07:59:32 +0700

    [BTA-10]:Initial project folder

--- views/partials/head.php
+++ views/partials/head.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Demo</title>
+    <title>Booking Ticket</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 </head>
 <body>
