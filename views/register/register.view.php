655e08b871309fb91d8bf9898bfdf5cda4d4cb87
Visal Sork <visalsork@Visals-MacBook-Air.local>
2023-01-24 20:21:59 +0700

    [BTA-21]:Update register view

--- views/register/register.view.php
+++ views/register/register.view.php
@@ -11,56 +11,64 @@
             <div>
                 <div class="flex -mx-3">
                     <div class="w-full px-3 mb-5">
-                        <label for="" class="text-white  px-1 font-medium">Username</label>
+                        <label for="username" class="text-white  px-1 font-medium">Username</label>
                         <div class="flex">
                             <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-email-outline text-gray-400 text-lg"></i></div>
-                            <input type="text" class="w-full -ml-10 p-1.5 border-2 border-gray-200 outline-none  font-thin" placeholder="Enter your username" required>
+                            <input type="text" class="w-full -ml-10 p-1.5 border-2 border-gray-200 outline-none  font-thin" placeholder="Enter your username" name="username">
                             </div>
                         </div>
                     </div>
                     <div class="flex -mx-3">
                         <div class="w-full px-3 mb-5">
-                            <label for="" class="text-white  px-1 font-medium">Email</label>
+                            <label for="email" class="text-white  px-1 font-medium">Email</label>
                             <div class="flex">
                                 <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-email-outline text-gray-400 text-lg"></i></div>
-                                <input type="email" class="w-full -ml-10 pr-3 p-1.5  border-2 border-gray-200 outline-none  font-thin" placeholder="Enter your email" required>
+                                <input type="email" class="w-full -ml-10 pr-3 p-1.5 border-2 border-gray-200 outline-none font-thin" placeholder="Enter your email" name="email">
                             </div>
                         </div>
                     </div>
                     <div class="flex -mx-3">
                         <div class="w-full px-3 mb-5">
-                            <label for="" class="text-white px-1 font-medium">Password</label>
+                            <label for="password" class="text-white px-1 font-medium">Password</label>
                             <div class="flex">
                                 <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-email-outline text-gray-400 text-lg"></i></div>
-                                <input type="password" class="w-full -ml-10 pr-3 p-1.5  border-2 border-gray-200 outline-none font-thin" placeholder="Enter your password" required>
+                                <input type="password" class="w-full -ml-10 pr-3 p-1.5  border-2 border-gray-200 outline-none font-thin" placeholder="Enter your password" name="password">
                             </div>
                         </div>
                     </div>
                     <div class="flex -mx-3">
                         <div class="w-full px-3 mb-5">
-                            <label for="" class="text-white   px-1 font-medium"> Confirm  password</label>
+                            <label for="confirm_password" class="text-white   px-1 font-medium"> Confirm  password</label>
                             <div class="flex">
                                 <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-lock-outline text-gray-400 text-lg"></i></div>
-                                <input type="password" class="w-full -ml-10 pr-3 p-1.5  border-2 border-gray-200 outline-none font-thin" placeholder="Enter your confirm password">
+                                <input type="password" class="w-full -ml-10 pr-3 p-1.5  border-2 border-gray-200 outline-none font-thin" placeholder="Enter your confirm password" name="confirm_password">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex -mx-3">
+                        <div class="w-full px-3 mb-5">
+                            <label for="date_of_birth" class="text-white px-1 font-medium text-blak-500">Date Of Birth</label>
+                            <div class="flex">
+                                <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-lock-outline text-gray-400 text-lg"></i></div>
+                                <input type="date" class="w-full -ml-10 pr-3 p-1.5  border-2 border-gray-200 outline-none font-thin" name="date_of_birth">
                             </div>
                         </div>
                     </div>
                     <div class="flex -mx-3">
                         <div class="w-full px-3 mb-9">
-                            <label for="" class="text-white px-1 font-medium text-blak-500">Address</label>
+                            <label for="address" class="text-white px-1 font-medium text-blak-500">Address</label>
                             <div class="flex">
                                 <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-lock-outline text-gray-400 text-lg"></i></div>
-                                <input type="text" class="w-full -ml-10 pr-3 p-1.5  border-2 border-gray-200 outline-none font-thin" placeholder="Enter your address ">
+                                <input type="text" class="w-full -ml-10 pr-3 p-1.5  border-2 border-gray-200 outline-none font-thin" placeholder="Enter your address" name="address">
                             </div>
                         </div>
                     </div>
                     <div class="flex -mx-3">
                         <div class="w-full px-3 mb-5">
-                            <button class=" bg-sky-500/50 hover:bg-sky-500/60 register block w-full  mx-auto text-white text-xl  font-bold p-2" >REGISTER</button>
+                            <button class="bg-sky-500/50 hover:bg-sky-500/60 register block w-full mx-auto text-white text-xl font-bold p-2" >REGISTER</button>
                             <p class="text-sm  text-gray-500 dark:text-white pt-1 pl-1">
-                            I have account.<a href="#"
-                                class="pl-1  font-lg text-primary-600 dark:text-primary-500 hover:underline hover:text-red-500">
-                                Log in </a>
+                                I have account.
+                                <a href="/"class="pl-1  font-lg text-primary-600 dark:text-primary-500 hover:underline hover:text-red-500">Log in </a>
                             </p>
                         </div>
                     </div>
