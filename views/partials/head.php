2dea2d499bdfd42e0989e19a87146e03d2e9fde3
Visal Sork <visalsork@Visals-MacBook-Air.local>
2023-01-24 11:24:21 +0700

    [BTA-19]: VIEW - Create a navbar on home page view

--- views/partials/head.php
+++ views/partials/head.php
@@ -8,4 +8,4 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 </head>
-<body>
+<body  style="background-image: linear-gradient(90deg,#041826,#082336,#143954,#093351,#092F4A,#072840,#05253A);">
