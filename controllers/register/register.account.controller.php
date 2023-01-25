a488c744c89eee5403eb9b49fda9feaaad7dc9c8
Visal Sork <visalsork@Visals-MacBook-Air.local>
2023-01-25 21:02:15 +0700

    [BTA-24]: Update validate register back-end

--- controllers/register/register.account.controller.php
+++ controllers/register/register.account.controller.php
@@ -1,15 +1,18 @@
 <?php
-
+session_start();
 require 'database/database.php';
 $username_error = $email_error = $password_error = $confirm_error = $date_error = $address_error = "";
 $form_valid = true;
 if (isset($_POST['submit'])) {
     if (isset($_POST['username'])) {
         $username = validateInput($_POST['username']);
-        if (strlen($username) > 5 && ctype_alnum($username)) {
+        if(ctype_alnum($username)){
             $_SESSION['username'] = $username;
+        } elseif (strlen($username) <= 5 ) {
+            $username_error= "Username must be at least 5 characters";
+            $form_valid=false;
         } elseif (empty($username)) {
-            $username_error = "Must fill username";
+            $username_error = "Username is required";
             $form_valid = false;
         } else {
             $username_error = "Username is invalid";
@@ -18,35 +21,30 @@ if (isset($_POST['submit'])) {
     }
     if (isset($_POST['email'])) {
         $email = validateInput($_POST['email']);
-        if (str_contains($email, '@') && filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $_SESSION['email'] = $email;
+        $_SESSION['email'] = $email;
+        if (!str_contains($email, '@') && !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $email_error = "Email is invalid";
+            $form_valid = false;
         } elseif (empty($email)) {
             $email_error = "Email is required";
-            $form_valid = false;
-        } else {
-            $email_error = "Email is invalid";
             $form_valid = false;
         }
     }
     if (isset($_POST['password'])) {
         $passowrd = validateInput($_POST['password']);
-        if (strlen($passowrd) > 8) {
-            $_SESSION['password'] = $passowrd;
+        if (strlen($passowrd) < 8) {
+            $passowrd="Password at least 8 length";
         } elseif (empty($passowrd)) {
-            $password_error = "Must fill password";
-            $form_valid = false;
-        } else {
-            $passowrd = "Password is invalid";
+            $password_error = "Password is required";
             $form_valid = false;
         }
     }
     if (isset($_POST['confirm_password'])) {
         $confirm_password = validateInput($_POST['confirm_password']);
         if ($confirm_password === $passowrd) {
-            $_SESSION['confirm_password'] = $confirm_password;
             $password_hash = password_hash($confirm_password,PASSWORD_BCRYPT);
-        } elseif (empty($passowrd)) {
-            $confirm_error = "Must fill password";
+        } elseif (empty($confirm_password)) {
+            $confirm_error = "Confirm Password is required";
             $form_valid = false;
         } else {
             $confirm_error = "Password does not match";
@@ -55,17 +53,20 @@ if (isset($_POST['submit'])) {
     }
     if (isset($_POST['date_of_birth'])) {
         $date = validateInput($_POST['date_of_birth']);
-        if (!empty($date)) {
-            $_SESSION['date'] = $date;
-        } else {
-            $date_error = "Date invalid";
+        $_SESSION['date'] = $date;
+        if (empty($date)) {
+            $date_error = "Date is required";
             $form_valid = false;
         }
     }
     if (isset($_POST['address'])) {
         $address = validateInput($_POST['address']);
-        if (!empty($address)) {
+        if(!empty($address)){
             $_SESSION['address'] = $address;
+        }
+        elseif (empty($address)) {
+            $address_error = "Address is required";
+            $form_valid = false;
         } else {
             $address_error = "Address invalid";
             $form_valid = false;
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,7 +1,17 @@
+<?php
+    session_start();
+    if(!empty($_SESSION['username'])){
+        $display="hidden";
+    }
+    else{
+        $display="";
+    }
+?>
+
 <nav class=" flex justify-between  border-gray-200 p-6"
         style="box-shadow: rgba(0, 0, 0, 0.45) 0px 25px 20px -20px;">
         <div class="nav-left mt-3 ">
-            <div class="flex flex-col  md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+            <div class="flex flex-col md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
                 <img class="h-10 w-10 -mt-2.5 rounded-full" src="assets/logo/cinema-logo.png">
                 <a href="/" class="block py-2 pr-4 pl-3 text-white  rounded md:bg-transparent md:text-white md:p-0 dark:text-white" aria-current="page">Home</a>
                 <a href="#" class="block py-2 pr-4 pl-3 text-white border-b border-white hover:bg-white md:hover:bg-transparent md:border-0 md:hover:text-white md:p-0 dark:text-white md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About Us</a>
@@ -10,6 +20,7 @@
         </div>
         <div class="nav-right mr-8 w-full md:block md:w-auto" id="mobile-menu  ">
             <div class="btn m-0 flex">
+                <a href="logout.php">Log out</a>
                 <form action="#" method="post" class="relative mx-auto text-gray-600 lg:block mr-8">
                     <input
                         class=" border-2 border-gray-300 bg-white w-96 h-10 pl-2 pr-8 rounded-full text-sm outline-white hover:outline-yellow-300"
@@ -24,9 +35,9 @@
                     </button>
                 </form>
                 <div class="btn">
-                    <button class=' bg-[#185b8b] hover:bg-[#2177B5] hover:underline m-0 py-2 pr-4 pl-3 text-white  rounded'>
+                    <button class='<?= $display; ?> bg-[#185b8b] hover:bg-[#2177B5] hover:underline m-0 py-2 pr-4 pl-3 text-white  rounded'>
                         <a href="/register">Register</a></button>
-                    <button class=' bg-[#0d3652] hover:bg-[#0C5384] hover:underline py-2 pr-4 pl-3 ml-auto text-white rounded '>
+                    <button class='bg-[#0d3652] hover:bg-[#0C5384] hover:underline py-2 pr-4 pl-3 ml-auto text-white rounded '>
                         <a href="/login"> Log in </a>
                     </button>
                 </div>
