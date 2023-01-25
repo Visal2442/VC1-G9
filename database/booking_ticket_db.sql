c0629359525d56f29696aa8363f919d24e0a3b3f
Visal Sork <visalsork@Visals-MacBook-Air.local>
2023-01-25 15:11:31 +0700

    [BTA-17]: Update database

--- database/booking_ticket_db.sql
+++ database/booking_ticket_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jan 24, 2023 at 04:32 AM
+-- Generation Time: Jan 25, 2023 at 09:10 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -20,6 +20,19 @@ SET time_zone = "+00:00";
 --
 -- Database: `booking_ticket_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `customer_id` int(11) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `date_of_birth` date NOT NULL,
+  `email_address` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -46,16 +59,32 @@ CREATE TABLE `shows` (
 --
 
 CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL,
+  `email_address` varchar(255) NOT NULL,
   `username` varchar(30) NOT NULL,
-  `email_address` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `role` varchar(30) NOT NULL
+  `type` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`email_address`, `username`, `password`, `type`) VALUES
+('dadada@gmail.com', 'visalvisal', '123456789', 0),
+('dem5@gmail.com', 'fafafafa', '123456789', 0),
+('dembo45@gmail.com', 'visalvisall', '123456789', 0),
+('fdsada@gmail.com', 'fdarerer', '1234512345', 0);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`customer_id`),
+  ADD KEY `email_address` (`email_address`);
 
 --
 -- Indexes for table `shows`
@@ -67,11 +96,17 @@ ALTER TABLE `shows`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
+  ADD PRIMARY KEY (`email_address`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `shows`
@@ -80,10 +115,14 @@ ALTER TABLE `shows`
   MODIFY `show_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `users`
+-- Constraints for dumped tables
 --
-ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for table `customers`
+--
+ALTER TABLE `customers`
+  ADD CONSTRAINT `email_address` FOREIGN KEY (`email_address`) REFERENCES `users` (`email_address`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
