490f888649d6822a7786d21ea0a5add6efc21b39
Visal Sork <visalsork@Visals-MacBook-Air.local>
2023-01-26 15:30:50 +0700

    [BTA-21]: Update register view

--- controllers/register/register.controller.php
+++ controllers/register/register.controller.php
@@ -1,5 +1,6 @@
 <?php
 
 require 'models/register.model.php';
+require 'models/login.model.php';
 
 require 'views/register/register.view.php';
--- views/register/register.view.php
+++ views/register/register.view.php
@@ -1,20 +1,19 @@
 <?php require "views/partials/head.php" ?>
 <?php require "views/partials/nav.php" ?>
-<?php require "views/partials/banner.php" ?>
 <?php require "controllers/register/register.account.controller.php" ?>
 
-<form action="#" method="post" class="min-w-screen min-h-screen flex items-center justify-center  py-5">
+<form action="#" method="post" class=" min-w-screen min-h-screen flex items-center justify-center  py-5">
     <div class="md:flex w-full h-1/4">
-        <div class="w-1/3 md:py-10 md:px-10 m-auto" style="background-color:#133954; box-shadow: rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px;">
+        <div class="register w-1/3 md:py-10 md:px-10 m-auto">
             <div class="text-center mb-3">
-                <h1 class="font-bold text-3xl text-white">REGISTER</h1>
+                <h1 class="font-bold text-4xl text-white">REGISTER</h1>
             </div>
             <div>
                 <div class="flex -mx-3">
                     <div class="w-full px-3 mb-5">
                         <label for="username" class="text-white px-1 font-medium">Username</label>
                         <div class="flex">
-                            <input type="text" class="w-full p-1.5 border-2 border-gray-200 outline-none  font-thin" placeholder="Enter your username" name="username">
+                            <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm  focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:focus:ring-blue-500 outline-0" placeholder="Enter your username" name="username">
                         </div>
                         <small class="text-red-500"> <?= $username_error; ?></small>
                         </div>
@@ -23,16 +22,17 @@
                         <div class="w-full px-3 mb-5">
                             <label for="email" class="text-white px-1 font-medium">Email</label>
                             <div class="flex">
-                                <input type="email" class="w-full pr-3 p-1.5 border-2 border-gray-200 outline-none font-thin" placeholder="Enter your email" name="email">
+                                <input type="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm  focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:focus:ring-blue-500 outline-0" placeholder="Enter your email" name="email">
                             </div>
-                            <small class="text-red-500"> <?= $email_error; ?></small>
+                            <small class="text-red-500"><?= $email_error; ?></small>
+                            <p class="text-red-500"><?= $isTaken_error;?></p>
                         </div>
                     </div>
                     <div class="flex -mx-3">
                         <div class="w-full px-3 mb-5">
                             <label for="password" class="text-white px-1 font-medium">Password</label>
                             <div class="flex">
-                                <input type="text" class="w-full pr-3 p-1.5 border-2 border-gray-200 outline-none font-thin" placeholder="Enter your password" name="password">
+                                <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm  focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:focus:ring-blue-500 outline-0" placeholder="Enter your password" name="password">
                             </div>
                             <small class="text-red-500"> <?= $password_error; ?></small>
                         </div>
@@ -41,7 +41,7 @@
                         <div class="w-full px-3 mb-5">
                             <label for="confirm_password" class="text-white px-1 font-medium"> Confirm  password</label>
                             <div class="flex">
-                                <input type="password" class="w-full pr-3 p-1.5 border-2 border-gray-200 outline-none font-thin" placeholder="Enter your confirm password" name="confirm_password">
+                                <input type="password" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm  focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:focus:ring-blue-500 outline-0" placeholder="Enter your confirm password" name="confirm_password">
                             </div>
                             <small class="text-red-500"> <?= $confirm_error; ?></small>
                         </div>
@@ -50,7 +50,7 @@
                         <div class="w-full px-3 mb-5">
                             <label for="date_of_birth" class="text-white px-1 font-medium text-blak-500">Date Of Birth</label>
                             <div class="flex">
-                                <input type="date" class="w-full pr-3 p-1.5 border-2 border-gray-200 outline-none font-thin" name="date_of_birth">
+                                <input type="date" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm  focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:focus:ring-blue-500 outline-0" name="date_of_birth">
                             </div>
                             <small class="text-red-500"> <?= $date_error; ?></small>
                         </div>
@@ -59,7 +59,7 @@
                         <div class="w-full px-3 mb-9">
                             <label for="address" class="text-white px-1 font-medium text-blak-500">Address</label>
                             <div class="flex">
-                                <input type="text" class="w-full pr-3 p-1.5 border-2 border-gray-200 outline-none font-thin" placeholder="Enter your address" name="address">
+                                <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm  focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:focus:ring-blue-500 outline-0" placeholder="Enter your address" name="address">
                             </div>
                             <small class="text-red-500"> <?= $address_error; ?></small>
                         </div>
@@ -67,9 +67,9 @@
                     <div class="flex -mx-3">
                         <div class="w-full px-3 mb-5">
                             <button class="bg-sky-500/50 hover:bg-sky-500/60 register block w-full mx-auto text-white text-xl font-bold p-2" type='submit' name="submit">REGISTER</button>
-                            <p class="text-sm text-gray-500 dark:text-white pt-1 pl-1">
+                            <p class="text-sm text-white">
                                 I have account.
-                                <a href="/"class="pl-1 font-lg text-primary-600 dark:text-primary-500 hover:underline hover:text-red-500">Log in </a>
+                                <a href="/login"class="pl-1 font-lg font-bold text-primary-600 dark:text-primary-500 hover:underline hover:text-red-500">Log in </a>
                             </p>
                         </div>
                     </div>
