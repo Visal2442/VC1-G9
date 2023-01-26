0319d0c32d54a018cfab1f8db88e03aadbb3d396
Visal Sork <visalsork@Visals-MacBook-Air.local>
2023-01-26 15:03:45 +0700

    [BTA-51]: Update banner

--- views/partials/banner.php
+++ views/partials/banner.php
@@ -1,3 +1,24 @@
-<header class="card mt-4">
 
-</header>
+<div class="slideshow-container">
+    <div class="mySlides fade">
+        <img src="https://www.majorcineplex.com.kh/load_file/ads/file_20224729104755.jpg" style="width:100%; height:70vh">
+    </div>
+    <div class="mySlides fade">
+        <img src="https://www.majorcineplex.com.kh/load_file/ads/file_20232106122123.jpg" style="width:100%;height:70vh">
+    </div>
+    <div class="mySlides fade">
+        <img src="https://www.majorcineplex.com.kh/load_file/ads/file_20224203044220.jpg" style="width:100%;height:70vh">
+    </div>
+    <div class="mySlides fade">
+        <img src="https://www.majorcineplex.com.kh/load_file/ads/file_20235206115238.png" style="width:100%;height:70vh">
+    </div>
+</div>
+
+<div style="text-align:center">
+    <span class="dot hover:bg-white" onclick="currentDiv(0)"></span>
+    <span class="dot hover:bg-white" onclick="currentDiv(1)"></span>
+    <span class="dot hover:bg-white" onclick="currentDiv(2)"></span>
+    <span class="dot hover:bg-white" onclick="currentDiv(3)"></span>
+</div>
+
+<script src="views/js/banner.js"></script>
