cd6653968b8f3a8a490400b4aa0894af69f25242
Visal Sork <visalsork@Visals-MacBook-Air.local>
2023-01-26 15:07:58 +0700

    [BTA-51]: Update banner

--- views/partials/head.php
+++ views/partials/head.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Booking Ticket</title>
+    <link rel="stylesheet" href="views/css/main.css">
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 </head>
