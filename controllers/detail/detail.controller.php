147407989a181bd9124292c43c2b8f2313d35dd1
Visal Sork <visalsork@Visals-MacBook-Air.local>
2023-01-28 09:30:33 +0700

    [BTA-45]: Create detail controller

--- controllers/detail/detail.controller.php
+++ controllers/detail/detail.controller.php
@@ -1,5 +1,10 @@
 <?php
-
-// require 'models/login.model.php';
+require 'database/database.php';
+require 'models/show.model.php';
+$id=$_GET["id"]?$_GET["id"]:null;
+if(isset($id)){
+    $show= getShowById($_GET["id"]);
+}
 
 require 'views/detail/detail.view.php';
+
