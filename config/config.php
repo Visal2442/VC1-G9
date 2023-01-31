ad6482441742c84c43c22f1659106d25d43fc1cf
Visal Sork <visalsork@Visals-MacBook-Air.local>
2023-01-31 08:23:22 +0700

    [BTA-22]:Update register  controller

--- config/config.php
+++
@@ -1,4 +0,0 @@
-<?php 
-session_destroy();
-session_unset();
-header('location:/');
--- views/profile/profile.view.php
+++ views/profile/profile.view.php
@@ -4,7 +4,7 @@
 
 <div class="flex p-5 items-center justify-center">
 	<div class=" flex flex-col items-center justify-center">
-		<!-- <?php
+		<?php
 			if (!isset($_SESSION['profile'])) {
 		?>
 			<img src="../../assets/profile/unnamed.png" class="h-52 w-52 rounded-full m-9">
@@ -15,7 +15,7 @@
 		<?php
 
 		}
-		?> -->
+		?>
 		<label class="block text-sm font-medium text-white dark:text-white" for="file_input">
 			<svg fill="none" class="w-10 h-10  -mt-24  ml-40 rounded-full p-2 bg-slate-900" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
 				<path stroke-linecap="round" stroke-linejoin="round" d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z"></path>
