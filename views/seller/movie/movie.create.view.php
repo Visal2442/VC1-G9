7c8cb7e1fbbb7da4fa5373458d56ee347fafb987
Visal Sork <visalsork@Visals-MacBook-Air.local>
2023-02-17 19:17:41 +0700

    [BTA-106]: Update edit movie view

--- views/seller/movie/movie.create.view.php
+++ views/seller/movie/movie.create.view.php
@@ -1,10 +1,8 @@
-<div class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto" id="createMovie" tabindex="-1" aria-hidden="true">
+<div class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto" id="createMovie">
     <div class="modal-dialog modal-lg relative w-auto pointer-events-none">
         <div class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
             <div class="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
-                <h5 class="text-xl font-medium leading-normal text-gray-800">
-                    Add movie
-                </h5>
+                <h5 class="text-xl font-medium leading-normal text-gray-800">Add movie</h5>
                 <button type="button" class="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body relative p-4">
