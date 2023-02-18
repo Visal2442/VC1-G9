99dc2824caed37526cc6af1c59944f5fda06ccb0
Visal Sork <visalsork@Visals-MacBook-Air.local>
2023-02-18 20:56:43 +0700

    [BTA-72]:Update edit movie

--- views/js/movie/movie.js
+++ views/js/movie/movie.js
@@ -38,6 +38,35 @@ $(document).ready(function () {
         }
     });
   });
+
+  // Update Movie
+  $("#edit_movie_form").on("submit", function (e) {
+    e.preventDefault();
+    let form_data = new FormData(this);
+      $.ajax({
+        url: "controllers/seller/movie/movie.update.controller.php",
+        method: "post",
+        data: form_data,
+        contentType: false,
+        processData: false,
+        success: function (data) {
+          if (data == "success") {
+            $("#editMovie").modal("hide");
+            displayMovie();
+          } else if (data == "Extension doesn't support") {
+            alert("You cannot uplaod file of this type");
+          } else if (data == "size_too_large") {
+            alert("Image is too large.");
+          } else if (data == "This movie is already exists") {
+            alert(data);
+          } else if (data == "Error pattern") {
+            alert("Inpu t does not match the pattern");
+          } else {
+            alert("Input cannot be empty!");
+          }
+        }
+    });
+  });
 });
 
 // Display Movies
